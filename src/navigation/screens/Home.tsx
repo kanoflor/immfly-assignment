470a5962fae202c54921648a724af0fdfd3c4a1e
kanoflor <kanoflordelis@gmail.com>
2025-09-29 17:30:12 +0200

    Implement product fetching and display in Home screen using FlatList

--- src/navigation/screens/Home.tsx
+++ src/navigation/screens/Home.tsx
@@ -1,11 +1,41 @@
 import { Button, Text } from "@react-navigation/elements";
-import { StyleSheet, View } from "react-native";
+import { useEffect, useState } from "react";
+import { FlatList, StyleSheet, View } from "react-native";
+
+// https://my-json-server.typicode.com/kanoflor/immfly-assignment
+
+type Product = {
+  id: string;
+  name: string;
+  priceEUR: number;
+  stock: number;
+  image: string;
+};
+
+const useProducts = (): Product[] => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch(
+      "https://my-json-server.typicode.com/kanoflor/immfly-assignment/products"
+    )
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  return products;
+};
 
 export function Home() {
+  const products = useProducts();
   return (
     <View style={styles.container}>
-      <Text>Screen 1</Text>
-      <Text>Open up 'src/App.tsx' to start working on your app!</Text>
+      <Text style={styles.title}>Screen 1</Text>
+      <FlatList
+        data={products}
+        renderItem={({ item }) => <Text>{item.name}</Text>}
+        keyExtractor={(item) => item.id}
+      />
       <Button screen="Cart">Go to Screen 2</Button>
     </View>
   );
@@ -14,8 +44,13 @@ export function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    padding: 20,
     gap: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 10,
   },
 });
